3f06c95c8c7ab60225ab63ba374a927102cc12e4
Paul Kat <loic@Loics-Mac.local>
2020-03-24 01:57:35 -0700

    Complete all tasks. 03/23/2020

--- aapoon/wsgi.py
+++ aapoon/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "aapoon.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "aapoon.settings.production")
 
 application = get_wsgi_application()
